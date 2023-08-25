f4614df4ef73a5609f71d6d1e5125f1a4da04263
khansazulfa <khansazulfa9@gmail.com>
2023-08-25 19:15:00 +0700

    add code dropdown, button

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,10 @@
-import { useRef } from "react";
-import {FaBars, FaCaretDown,FaTimes} from "react-icons/fa";
-import {Link} from "react-router-dom";
-import "./NavBar.css" ;
+import { useRef, useState } from "react";
+import { FaBars, FaCaretDown, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import { navItems, serviceDropdown } from "./NavItems";
+import Dropdown from "./Dropdown";
+import Button from "../Button";
 
 
 function NavBar() {
@@ -11,40 +14,57 @@ function NavBar() {
     navRef.current.classList.toggle("responsive_nav");
   }
 
+  const [dropdown, setDropdown] = useState(false);
+
   return (
     <div className="container">
       <nav ref={navRef} className="navbar">
-       <img className="imgLogo" src="src/assets/images/logo.png" alt="logo" />
-        <ul className="nav-links">
-        <Link to="/" className="home active">
-          <li>Home</li>
+        <Link to="/" className="navbar-logo">
+          <img className="imgLogo" src="src/assets/images/logo.png" alt="logo" />
         </Link>
-        <Link to="/tourism" className="tourism">
-          <li>Tourism</li>
-        </Link>
-        <Link to="/category" className="category">
-          <li>Category
-          <FaCaretDown />
-          </li>
-        </Link>
-        <Link to="/login" className="login">
+        <ul className="nav-items">
+          {navItems.map((item) => {
+            if (item.title === "Category") {
+              return (
+                <li
+                  key={item.id}
+                  className={item.cName}
+                  onMouseEnter={() => setDropdown(true)}
+                  onMouseLeave={() => setDropdown(false)}
+                >
+                  <Link to={item.path}>{item.title} <FaCaretDown /></Link>
+                  {dropdown && <Dropdown />}
+
+                </li>
+              );
+            }
+            return (
+              <li key={item.id} className={item.cName}>
+                <Link to={item.path}>{item.title}</Link>
+              </li>
+            );
+          })}
+
+        </ul>
+        <Button />
+
+        {/* <Link to="/login" className="login">
           <li>Login</li>
         </Link>
-        <Link to="/signup" className="signup">
+        <Link to="/register" className="signup">
           <li>Sign Up</li>
         </Link>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
-        </button>
-      </ul>
+        </button> */}
+
       </nav>
-    <button onClick={showNavbar} className="nav-btn">
-      <FaBars />
-    </button>
+      <button onClick={showNavbar} className="nav-btn">
+        <FaBars />
+      </button>
     </div>
-   
-    )
-  }
-  
-  export default NavBar;
-  
+
+  )
+}
+
+export default NavBar;
