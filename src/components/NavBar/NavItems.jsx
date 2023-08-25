54d8aa79ed2e91da475c817134fd377a2c34f150
khansazulfa <khansazulfa9@gmail.com>
2023-08-25 19:06:26 +0700

    add file navitems.jsx

---
+++ src/components/NavBar/NavItems.jsx
@@ -0,0 +1,46 @@
+export const navItems = [
+    {
+        id: 1,
+        title: "Home",
+        path: "./",
+        cName: "nav-item",
+    },
+    {
+        id: 2,
+        title: "Tourism",
+        path: "./tourism",
+        cName: "nav-item",
+    },
+    {
+        id: 3,
+        title: "Category",
+        path: "./category",
+        cName: "nav-item",
+    },
+];
+export const serviceDropdown = [
+    {
+        id: 1,
+        title: "Mountains",
+        path: "./mountains",
+        cName: "submenu-item",
+    },
+    {
+        id: 2,
+        title: "Maritim",
+        path: "./maritim",
+        cName: "submenu-item",
+    },
+    {
+        id: 3,
+        title: "Culture",
+        path: "./culture",
+        cName: "submenu-item",
+    },
+    {
+        id: 4,
+        title: "Education",
+        path: "./education",
+        cName: "submenu-item",
+    }
+];
