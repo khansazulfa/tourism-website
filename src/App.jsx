14907a7cd84e4f1a7b33d330c3afb312937fc212
khansazulfa <khansazulfa9@gmail.com>
2023-08-25 19:09:27 +0700

    add import for login n signup

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import MyCard from './components/MyCard'
 import { Routes, Route, Outlet } from "react-router-dom"
 import Home from './page/Home'
 import Login from './page/Login'
+import SignUp from './page/SignUp'
 import NavBar from './components/NavBar'
 
 
 function Header() {
   return (
-    <>    
+    <>
       <Outlet />
-      
+
     </>
   )
 }
@@ -18,15 +19,16 @@ function Header() {
 function App() {
 
   return (
-    
+
     <Routes>
-       <Route path='/' Component={Header}>
+      <Route path='/' Component={Header}>
         <Route path='/' Component={Home} />
-        
+
       </Route>
       <Route path='/login' Component={Login}></Route>
-      </Routes>
-  
+      <Route path='/register' Component={SignUp}></Route>
+    </Routes>
+
   )
 }
 
